b8ca4b78ea7b341d4e37be174f41d14ed54f6029
dkasprzak <dominik.kasprzak1408@gmail.com>
2024-10-27 14:29:41 +0100

    Initial commit

---
+++ PlaceCheck.Backend/PlaceCheck.WebApi/Program.cs
@@ -0,0 +1,48 @@
+using Serilog;
+
+var APP_NAME = "PlaceCheck.WebApi";
+
+Log.Logger = new LoggerConfiguration()
+    .Enrich.WithProperty("Application", APP_NAME)
+    .Enrich.WithProperty("MachineName", Environment.MachineName)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, services, configuration) => configuration
+    .Enrich.WithProperty("Application", APP_NAME)
+    .Enrich.WithProperty("MachineName", Environment.MachineName)
+    .ReadFrom.Configuration(context.Configuration)
+    .ReadFrom.Services(services)
+    .Enrich.FromLogContext());
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(o =>
+{
+    o.CustomSchemaIds(x =>
+    {
+        var name = x.FullName;
+        if (name != null)
+        {
+            name = name.Replace("+", "_");
+        }
+        return name;
+    });
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.Run();
